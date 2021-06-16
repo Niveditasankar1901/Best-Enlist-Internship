bfcc085770d5849bf4db775e7998c0e08df37fb9
Nivedita <53997811+Niveditasankar1901@users.noreply.github.com>
2021-06-16 06:53:02 +0530

    Add files via upload

---
+++ day4-vari&data type.py
@@ -0,0 +1,37 @@
+#Day 4
+#1)Create three variables (a,b,c) 
+a=120
+b=12
+c=90
+
+#a) Divide a by 10
+a=a/10
+print(a)
+#Output:- 12.0
+
+#b) Multiply b by 50
+b=b*50
+print(b)
+#Output:- 600
+
+#c) Add c value by 60
+c=c+60
+print(c)
+#Output:- 150
+
+#2)Create a String variable of 5 characters and replace the 3rd character with G
+x="PEACE"
+z=x[2]
+x=x.replace(z,"G")
+print(x)
+#Output:- PEGCE
+
+#3)Create two values of int,float data type & convert the vise versa
+
+m=23.7098
+print(int(m))
+#Output:- 23
+
+n=15
+print(float(n))
+#Output:- 15.0
